5fabda8033c0794cc0c0911ea51867ddc6e82079
Dmitry Kirshin <dmitry_wattt@mail.ru>
2023-10-16 20:13:54 +0300

    PCB-7 раскрасил попапы

--- frontend/pcb_front/src/landing/SignInPopUp.js
+++ frontend/pcb_front/src/landing/SignInPopUp.js
@@ -8,11 +8,11 @@ export default function SignInPopUp({closeFunc}) {
                     <div className='popup__logo'></div>
                     <h2 className='popup__title'>Название</h2>
                 </div>
-                <form className='form_type-signIn' name='form'>
+                <form className='form form_type-signIn' name='form'>
                     <input name="login" type="text" placeholder="Логин" className="form__text form__text_type_login" id="login-input" required minLength="2" maxLength="30"></input>
-                    <span></span>
+                    <p className='form__caption'>Введите логин</p>
                     <input name="password" type="text" placeholder="Пароль" className="form__text form__text_type_password" id="password-input" required minLength="8" maxLength="30"></input>
-                    <span></span>
+                    <p className='form__caption'>Введите пароль</p>
                 </form>
                 <button className="popup__btn popup__enter-btn" type="submit" arial-label="Войти">Войти</button>
                 <button className="popup__cancel-btn" type="button" arial-label="Отмена" onClick = {() => closeFunc()}>Отмена</button>
--- frontend/pcb_front/src/landing/SignUpPopUp.js
+++ frontend/pcb_front/src/landing/SignUpPopUp.js
@@ -10,11 +10,11 @@ export default function SignUpPopUp({closeFunc}) {
                 </div>
                 <form className='form_type-signUp' name='form'>
                     <input name="login" type="text" placeholder="Логин" className="form__text form__text_type_login" id="login-input" required minLength="2" maxLength="30"></input>
-                    <span></span>
+                    <p className='form__caption'>Придумайте логин</p>
                     <input name="password" type="text" placeholder="Пароль" className="form__text form__text_type_password" id="password-input" required minLength="8" maxLength="30"></input>
-                    <span></span>
+                    <p className='form__caption'>Пароль должен содержать не менее 6 символов</p>
                     <input name="sampPassword" type="text" placeholder="Повторите пароль" className="form__text form__text_type_samePassword" id="samePassword-input" required minLength="8" maxLength="30"></input>
-                    <span></span>
+                    <p className='form__caption'>Повторите пароль</p>
                 </form>
                 <div className="popup__checkbox">
                     <input type="checkbox" className="checkbox"></input>
