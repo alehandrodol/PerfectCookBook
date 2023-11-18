c7886609d3f54ddb20356399eddf1a703202992a
Константин Романов <const.romanov.dev@gmail.com>
2023-11-18 17:58:03 +0300

    PCB-25

--- backend/handlers/images.py
+++ backend/handlers/images.py
@@ -11,12 +11,15 @@ router = APIRouter()
 
 @router.post("/images/")
 async def create_image(file: UploadFile):
+    # изображения кладутся в папку images которая (по идее) находится в корне проекта
+    # если он запускается из PerfectCookBook/backend, как в инструкции написано
     out_file_path = os.path.join(os.getcwd(), "..", "images")
     out_file_path = os.path.abspath(out_file_path)
 
     if not os.path.exists(out_file_path):
         await aios.mkdir(out_file_path)
 
+    # название файла - хеш его кусочка и расширение оригинала
     filename = hashlib.md5(await file.read(8192)).hexdigest() + os.path.splitext(file.filename)[1]
     out_file_path = os.path.join(out_file_path, filename)
 
@@ -24,6 +27,7 @@ async def create_image(file: UploadFile):
         content = await file.read()
         await out_file.write(content)
 
+    # возвращаем урл относительно домена по которому можно будет получить изображение
     return {"path": "/images/" + filename}
 
 
